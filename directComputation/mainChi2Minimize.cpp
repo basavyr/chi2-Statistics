271f13a07f69874d255248aadae0257f34bd6271
Robert Poenaru <robert.poenaru@protonmail.ch>
2019-09-07 19:03:35 +0300

    adds energy comparison function
    Fixes formula of CHI2
    Makes experimental data global
    Enlarges interval for particle potential
    Creates output data for each energy band in separate files

--- directComputation/mainChi2Minimize.cpp
+++ directComputation/mainChi2Minimize.cpp
@@ -9,6 +9,16 @@ using namespace std;
 
 ofstream output("outputData.dat");
 
+const int dim1 = 21, dim2 = 17, dim3 = 14, dim4 = 10;
+double tsd1[21] = {0.1966, 0.4597, 0.7746, 1.1609, 1.6112, 2.1265, 2.7051, 3.3441, 4.0411, 4.7937, 5.5992, 6.457, 7.3667, 8.3293, 9.3458, 10.4169, 11.5431, 12.7224, 13.9491, 15.2181, 16.5221};
+double tsd2[17] = {1.3394, 1.7467, 2.2184, 2.7527, 3.3484, 4.003, 4.7143, 5.4805, 6.3004, 7.1733, 8.0998, 9.08, 10.1147, 11.2036, 12.3466, 13.5441, 14.7911};
+double tsd3[14] = {2.1237, 2.6293, 3.1973, 3.8243, 4.5094, 5.2506, 6.0465, 6.8963, 7.7988, 8.7546, 9.7638, 10.8268, 11.9392, 13.0861};
+double tsd4[10] = {4.58, 5.2251, 5.9273, 6.6819, 7.4919, 8.3573, 9.2778, 10.2535, 11.2851, 12.3701};
+double spin1Exp[dim1] = {8.5, 10.5, 12.5, 14.5, 16.5, 18.5, 20.5, 22.5, 24.5, 26.5, 28.5, 30.5, 32.5, 34.5, 36.5, 38.5, 40.5, 42.5, 44.5, 46.5, 48.5};
+double spin2Exp[dim2] = {13.5, 15.5, 17.5, 19.5, 21.5, 23.5, 25.5, 27.5, 29.5, 31.5, 33.5, 35.5, 37.5, 39.5, 41.5, 43.5, 45.5};
+double spin3Exp[dim3] = {16.5, 18.5, 20.5, 22.5, 24.5, 26.5, 28.5, 30.5, 32.5, 34.5, 36.5, 38.5, 40.5, 42.5};
+double spin4Exp[dim4] = {23.5, 25.5, 27.5, 29.5, 31.5, 33.5, 35.5, 37.5, 39.5, 41.5};
+
 double inertiaMoment(double izero, double beta, double gamma, int k)
 {
     double rez, pi = 3.141592654;
@@ -117,7 +127,7 @@ double Omega1(double spin, double oddSpin, double izero, double beta, double gam
 double Omega2(double spin, double oddSpin, double izero, double beta, double gamma, double particlePotential)
 {
     double rez;
-    rez = sqrt(0.5 * (-1.0 * bigTerm1(spin, oddSpin, izero, beta, gamma, particlePotential) + pow(pow(bigTerm1(spin, oddSpin, izero, beta, gamma, particlePotential), 2) - 4.0 * bigTerm2(spin, oddSpin, izero, beta, gamma, particlePotential), 0.5)));
+    rez = sqrt(0.5 * (-bigTerm1(spin, oddSpin, izero, beta, gamma, particlePotential) + pow(pow(bigTerm1(spin, oddSpin, izero, beta, gamma, particlePotential), 2) - 4.0 * bigTerm2(spin, oddSpin, izero, beta, gamma, particlePotential), 0.5)));
     return rez;
 }
 
@@ -194,20 +204,11 @@ double chiSuqaredMinimize(double izero, double particlePotential)
     int count = 0;
     double oddSpin = 6.5, secondSpin = 4.5;
     double sum1 = 0.0, sum2 = 0.0, sum3 = 0.0, sum4 = 0.0;
-    const int dim1 = 21, dim2 = 17, dim3 = 14, dim4 = 10;
     double beta, gamma;
     beta = 0.38;
     gamma = 17.0 * pi / 180.0;
 
     //experimental data
-    double tsd1[21] = {0.1966, 0.4597, 0.7746, 1.1609, 1.6112, 2.1265, 2.7051, 3.3441, 4.0411, 4.7937, 5.5992, 6.457, 7.3667, 8.3293, 9.3458, 10.4169, 11.5431, 12.7224, 13.9491, 15.2181, 16.5221};
-    double tsd2[17] = {1.3394, 1.7467, 2.2184, 2.7527, 3.3484, 4.003, 4.7143, 5.4805, 6.3004, 7.1733, 8.0998, 9.08, 10.1147, 11.2036, 12.3466, 13.5441, 14.7911};
-    double tsd3[14] = {2.1237, 2.6293, 3.1973, 3.8243, 4.5094, 5.2506, 6.0465, 6.8963, 7.7988, 8.7546, 9.7638, 10.8268, 11.9392, 13.0861};
-    double tsd4[10] = {4.58, 5.2251, 5.9273, 6.6819, 7.4919, 8.3573, 9.2778, 10.2535, 11.2851, 12.3701};
-    double spin1Exp[dim1] = {8.5, 10.5, 12.5, 14.5, 16.5, 18.5, 20.5, 22.5, 24.5, 26.5, 28.5, 30.5, 32.5, 34.5, 36.5, 38.5, 40.5, 42.5, 44.5, 46.5, 48.5};
-    double spin2Exp[dim2] = {13.5, 15.5, 17.5, 19.5, 21.5, 23.5, 25.5, 27.5, 29.5, 31.5, 33.5, 35.5, 37.5, 39.5, 41.5, 43.5, 45.5};
-    double spin3Exp[dim3] = {16.5, 18.5, 20.5, 22.5, 24.5, 26.5, 28.5, 30.5, 32.5, 34.5, 36.5, 38.5, 40.5, 42.5};
-    double spin4Exp[dim4] = {23.5, 25.5, 27.5, 29.5, 31.5, 33.5, 35.5, 37.5, 39.5, 41.5};
 
     double temp;
     bool ok = 1;
@@ -215,7 +216,7 @@ double chiSuqaredMinimize(double izero, double particlePotential)
     {
         for (int i = 0; i < dim1 && ok; ++i)
         {
-            temp = pow((double)(tsd1[i] - energyBand1(spin1Exp[i], oddSpin, izero, beta, gamma, particlePotential)), 2.0);
+            temp = pow((double)(tsd1[i] - energyBand1(spin1Exp[i], oddSpin, izero, beta, gamma, particlePotential)), 2.0) / tsd1[i];
             if (isnan(temp))
             {
                 ok = false;
@@ -229,7 +230,7 @@ double chiSuqaredMinimize(double izero, double particlePotential)
         }
         for (int i = 0; i < dim2 && ok; ++i)
         {
-            temp = pow((double)(tsd2[i] - energyBand2(spin2Exp[i], oddSpin, izero, beta, gamma, particlePotential)), 2.0);
+            temp = pow((double)(tsd2[i] - energyBand2(spin2Exp[i], oddSpin, izero, beta, gamma, particlePotential)), 2.0) / tsd2[i];
             if (isnan(temp))
             {
                 ok = false;
@@ -243,7 +244,7 @@ double chiSuqaredMinimize(double izero, double particlePotential)
         }
         for (int i = 0; i < dim3 && ok; ++i)
         {
-            temp = pow((double)(tsd3[i] - energyBand3(spin3Exp[i], oddSpin, izero, beta, gamma, particlePotential)), 2.0);
+            temp = pow((double)(tsd3[i] - energyBand3(spin3Exp[i], oddSpin, izero, beta, gamma, particlePotential)), 2.0) / tsd3[i];
             if (isnan(temp))
             {
                 ok = false;
@@ -257,7 +258,7 @@ double chiSuqaredMinimize(double izero, double particlePotential)
         }
         for (int i = 0; i < dim4 && ok; ++i)
         {
-            temp = pow((double)(tsd4[i] - energyBand4(spin4Exp[i], oddSpin, izero, beta, gamma, particlePotential)), 2.0);
+            temp = pow((double)(tsd4[i] - energyBand4(spin4Exp[i], oddSpin, izero, beta, gamma, particlePotential)), 2.0) / tsd4[i];
             if (isnan(temp))
             {
                 ok = false;
@@ -303,30 +304,65 @@ static bool customCompare(const dp &x, const dp &y)
     return x.chi2 < y.chi2;
 }
 
+ofstream tsd1out("/mnt/c/Users/rober/OneDrive/Desktop/PIPELINE/DevWorkspace/GO_codes/go/src/plotMaker/tsd1.dat");
+ofstream tsd2out("/mnt/c/Users/rober/OneDrive/Desktop/PIPELINE/DevWorkspace/GO_codes/go/src/plotMaker/tsd2.dat");
+ofstream tsd3out("/mnt/c/Users/rober/OneDrive/Desktop/PIPELINE/DevWorkspace/GO_codes/go/src/plotMaker/tsd3.dat");
+ofstream tsd4out("/mnt/c/Users/rober/OneDrive/Desktop/PIPELINE/DevWorkspace/GO_codes/go/src/plotMaker/tsd4.dat");
+
+void makeEnergyData(double izero, double particlePotential)
+{
+    vector<double> energy1, energy2, energy3, energy4;
+    const double pi = 3.141592654;
+    double beta = 0.38, gamma = 17 * pi / 180.0, oddSpin = 6.5, secondSpin = 4.5;
+    for (int i = 0; i < dim1; ++i)
+    {
+        energy1.push_back(energyBand1(spin1Exp[i], oddSpin, izero, beta, gamma, particlePotential));
+        tsd1out << spin1Exp[i] << "," << tsd1[i] << "," << energy1.at(i) << endl;
+    }
+    for (int i = 0; i < dim2; ++i)
+    {
+        energy2.push_back(energyBand2(spin2Exp[i], oddSpin, izero, beta, gamma, particlePotential));
+        tsd2out << spin2Exp[i] << "," << tsd2[i] << "," << energy2.at(i) << endl;
+    }
+    for (int i = 0; i < dim3; ++i)
+    {
+        energy3.push_back(energyBand3(spin3Exp[i], oddSpin, izero, beta, gamma, particlePotential));
+        tsd3out << spin3Exp[i] << "," << tsd3[i] << "," << energy3.at(i) << endl;
+    }
+    for (int i = 0; i < dim4; ++i)
+    {
+        energy4.push_back(energyBand4(spin4Exp[i], secondSpin, izero, beta, gamma, particlePotential));
+        tsd4out << spin4Exp[i] << "," << tsd4[i] << "," << energy4.at(i) << endl;
+    }
+}
+
 int main()
 {
     vector<dp> chi2Array;
     dp result;
     double minValue = 123456789.0, pi = 3.141592654;
     double w1, w2, w3;
-    for (double i = 45; i <= 55; i+=0.1)
+    for (double i = 45; i <= 55; i += 0.1)
     {
-        for (double v = 0.01; v <= 10.0; v += 0.1)
+        for (double v = 0.01; v <= 10.0; v += 1)
         {
             if (chiSuqaredMinimize((double)i, v))
                 chi2Array.push_back(adddata((double)i, v, chiSuqaredMinimize((double)i, v)));
         }
     }
     //sort(chi2Array.begin(), chi2Array.end(), [](vector<dp> &a1, vector<dp> &a2) { return });
-    sort(chi2Array.begin(), chi2Array.end(), customCompare);
+    //sort(chi2Array.begin(), chi2Array.end(), customCompare);
     for (int i = 0; i < chi2Array.size(); ++i)
         output
             << chi2Array.at(i).izero << " " << chi2Array.at(i).particlePotential << " " << chi2Array.at(i).chi2 << "\n";
     //SHOW MINIMUM using lambda function
-    /*  auto min_chi2 = std::minmax_element(chi2Array.begin(), chi2Array.end(),
+    auto min_chi2 = std::minmax_element(chi2Array.begin(), chi2Array.end(),
                                         [](dp const &el1, dp const &el2) { return el1.chi2 < el2.chi2; });
-    cout << min_chi2.first->chi2 << endl; */
+    cout << min_chi2.first->izero << " " << min_chi2.first->particlePotential << " " << min_chi2.first->chi2 << endl;
+    makeEnergyData(min_chi2.first->izero, min_chi2.first->particlePotential);
     //computeMinimum(chi2Array, &result);
-    cout << "Program finished... ----> " << chi2Array.at(0).chi2 << endl;
+    //cout << "Program finished... ----> " << chi2Array.at(0).chi2 << endl;
+    cout << "Program finished... ----> ";
+    cout << endl;
     return 0;
 }
